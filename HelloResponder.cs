1c5ec993e5b3fa04ba4d2aeec0b77a6d17b73257
whiterabb17 <blackrabb17@protonmail.com>
2022-05-19 23:59:12 +0200

    Initial commit

---
+++ HelloResponder.cs
@@ -0,0 +1,64 @@
+﻿using MargieBot;
+using System;
+using System.Text;
+
+namespace Slakking
+{
+    public class HelloResponder : IResponder
+    {
+        public bool CanRespond(ResponseContext context)
+        {
+            return context.Message.MentionsBot
+                  && !context.BotHasResponded
+                  && context.Message.Text.ToLower().Contains("hello");
+        }
+
+        public BotMessage GetResponse(ResponseContext context)
+        {
+            var builder = new StringBuilder();
+            builder.Append("Hello ").Append(context.Message.User.FormattedUserID);
+
+            return new BotMessage { Text = builder.ToString() };
+        }
+
+        public static void SSend(string WebHook, string content)
+        {
+            string urlWithAccessToken = WebHook;
+
+            SlackClient client = new SlackClient(urlWithAccessToken);
+
+            client.PostMessage(username: Variables.ResponderAgent,
+                       text: ":: Notification message to be delivered to channel: " + DateTime.UtcNow.ToString() + "\r\n" + content,
+                       channel: "#BotChannel");
+        }
+        public BotMessage CommandSender(ResponseContext context)
+        {
+            string responder = null;
+            var builder = new StringBuilder();
+            string respondee = context.Message.User.FormattedUserID;
+            string[] _ = context.Message.Text.Split(':');
+            if (context.Message.MentionsBot && !context.BotHasResponded && !context.Message.Text.ToLower().Contains(respondee + " cmd:"))
+            {
+                foreach (Commands cmd in CommandHolder.CommandList)
+                {
+                    if (_[1] == cmd.Command1)
+                    {
+                        foreach (string file in Functions.getFiles())
+                        {
+                            responder += file + Environment.NewLine;
+                        }
+                        builder.Append(responder);
+                    }
+                }
+            }
+
+            SlackClient client = new SlackClient(Variables.urlWithAccessToken);
+
+            client.PostMessage(username: Variables.ResponderAgent,
+                       text: ":: New Command recieved from Controller at " + DateTime.UtcNow.ToString() + "\r\n" + "Command specified: " + _[1] + "\r\n" + "Send to client: " + respondee,
+                       channel: "#kingdomsfoundation");
+
+            return new BotMessage { Text = builder.ToString() };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using MargieBot;
+using System;
+using System.Configuration;
+
+namespace Slakking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CommandHolder.CommandList.Add(new Commands()
+            {
+                Command1 = "listFiles",
+                Command2 = "listFolders",
+                Command3 = "getUuid",
+                Command4 = "getSysName"
+            });
+            var container = new WindsorContainer();
+            container.Register(Component.For<IResponder>().ImplementedBy<HelloResponder>());
+
+            var bot = new Bot();
+            var responders = container.ResolveAll<IResponder>();
+            foreach (var responder in responders)
+            {
+                bot.Responders.Add(responder);
+            }
+            var connect = bot.Connect(ConfigurationManager.AppSettings["SlackBotApiToken"]);
+
+            while (Console.ReadLine() != "close") { }
+        }
+    }
+}
